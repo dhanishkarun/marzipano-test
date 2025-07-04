4f55fef22c51cff5f7622e6e52210b9420f0829e
dhanishkarun <35343771+dhanishkarun@users.noreply.github.com>
2025-07-04 23:36:54 +0530

    s
    s

---
+++ app-files/data.js
@@ -0,0 +1,62 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-20250704_224218_782",
+      "name": "20250704_224218_782",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.0465845798552635,
+        "pitch": 0.004651440127064532,
+        "fov": 1.3401592523986738
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0010818504350851,
+          "pitch": -0.053605047437251585,
+          "rotation": 6.283185307179586,
+          "target": "0-20250704_224218_782"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0006124505729446,
+          "pitch": -0.16326302836453266,
+          "title": "TV Unit",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.07681914436024684,
+          "pitch": -0.3425989057051311,
+          "title": "Balcony",
+          "text": "Balcony"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
